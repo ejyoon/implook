40dc563ae7b7a4bd5d0fa75f9c1a3caa79534f4d
ejyoon <ejyoon@stanford.edu>
2013-12-17 13:27:33 -0700

    moved into new folder

---
+++ implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook-getResults.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+pushd /Users/ericang/implookGIT/mturk/aws-mturk-clt-1.3.1/bin
+./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/ericang/implookGIT/implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook.success -outputfile /Users/ericang/implookGIT/implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook.results
+popd
---
+++ implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook-postHIT.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+pushd /Users/ericang/implookGIT/mturk/aws-mturk-clt-1.3.1/bin
+./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /Users/ericang/implookGIT/implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook -input /Users/ericang/implookGIT/implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook.input -question /Users/ericang/implookGIT/implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook.question -properties /Users/ericang/implookGIT/implook-VERSIONS/0-CONTROL_131216/0-CONTROL-IGNO/eyimplook.properties -maxhits 1
+popd
